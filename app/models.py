be64d831568e495c0b2f40365f6a3e441d5f65e5
ritik <ritikjain666@gmail.com>
2025-11-28 14:29:55 +0530

    added pre-save restock logic, read restock threshold from config

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from mongoengine import Document, StringField, FloatField, IntField, BooleanField
+from mongoengine import Document, StringField, FloatField, IntField, BooleanField, signals
 from config import Config
+
+RESTOCK_THRESHOLD = Config.RESTOCK_THRESHOLD
 
 class Product(Document):
     meta = {"collection": "products"}
@@ -20,3 +22,25 @@ class Product(Document):
             "available_quantity": int(self.available_quantity),
             "need_restock": bool(self.need_restock),
         }
+
+
+def calculate_need_restock(total_quantity: int, available_quantity: int) -> bool:
+    try:
+        total_quantity = int(total_quantity)
+        available_quantity = int(available_quantity)
+    except (TypeError, ValueError):
+        return True
+
+    if total_quantity <= 0:
+        return True
+    return available_quantity < (total_quantity * RESTOCK_THRESHOLD)
+
+
+def pre_save_product(sender, document, **kwargs):
+    if document.available_quantity > document.total_quantity:
+        document.available_quantity = document.total_quantity
+
+    document.need_restock = calculate_need_restock(document.total_quantity, document.available_quantity)
+
+
+signals.pre_save.connect(pre_save_product, sender=Product)
